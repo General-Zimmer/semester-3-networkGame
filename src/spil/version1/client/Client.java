5f2334dff29815f77870d5a7e2e41b3397ee6450
victor <au670070@uni.au.dk>
2024-03-06 11:38:06 +0100

    aendringer 2

--- src/spil/version1/client/Client.java
+++ src/spil/version1/client/Client.java
@@ -1,21 +1,46 @@
 package spil.version1.client;
 
+import spil.version1.gamefiles.ConcurrentArrayList;
 import spil.version1.gamefiles.GameLogic;
 import spil.version1.gamefiles.Player;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.Socket;
 
 // Denne er kun medtaget til Test-formål, skal IKKE anvendes.
-public class Client {
+public class Client{
 	public static Player me;
 	public static final GameLogic localLogic = new GameLogic();
 	public static void main(String argv[]) throws Exception{
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket= new Socket("localhost",1337);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
 		System.out.println("Indtast spillernavn");
 		String navn = null;
 		navn = inFromUser.readLine();
+
+		outToServer.writeBytes("arnold tilmed "+ navn+"\n");
+
+		boolean wait = true;
+		while(wait){//vente på svar fra server
+			String message = inFromServer.readLine();
+
+			if(message.equals(navn+ " tilmeldt")){
+				wait=false;
+			}else if(message.equals(navn+ " afvist")){
+				System.out.println("Server afviste spiller");
+				return;
+			}
+		}
+
+		String stringRead = inFromServer.readLine();
+		FileInputStream inputStream = new FileInputStream(stringRead);
+
+		ObjectInputStream objectMap = new ObjectInputStream(inputStream);
+
+		ConcurrentArrayList gameState = (ConcurrentArrayList) objectMap.readObject();
 
 		GuiThread gui = new GuiThread();
 		gui.start();
