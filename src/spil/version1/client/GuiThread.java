093d583021ca928a3c3a9309379cd95d19971e43
victor <au670070@uni.au.dk>
2024-03-08 08:44:25 +0100

    slettet lidt

--- src/spil/version1/client/GuiThread.java
+++ src/spil/version1/client/GuiThread.java
@@ -14,16 +14,6 @@ import java.io.InputStreamReader;
 public class GuiThread extends Thread{
 	public static Player me;
 	public void run(){
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Indtast spillernavn");
-		String navn = null;
-		try {
-			navn = inFromUser.readLine();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		me= GameLogic.makePlayer(navn);
-
 		Application.launch(Gui.class);
 	}
 }
