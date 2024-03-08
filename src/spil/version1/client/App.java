90a0162048900040e81fc4b06ae6f2e960b2abff
GZimmer <kristianz1000@gmail.com>
2024-03-08 09:47:19 +0100

    Added interface and made gameLogic not static

--- src/spil/version1/client/App.java
+++ src/spil/version1/client/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import spil.version1.gamefiles.GameLogic;
 import spil.version1.gamefiles.Gui;
 import spil.version1.gamefiles.Player;
+import spil.version1.interfaces.IEGameLogic;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,12 +13,15 @@ import java.io.InputStreamReader;
 
 public class App {
 	public static Player me;
+	public static IEGameLogic gameLogic;
 	public static void main(String[] args) throws Exception{	
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Indtast spillernavn");
+		gameLogic = new GameLogic();
 		String navn = inFromUser.readLine();
-		me= GameLogic.makePlayer(navn);
-		GameLogic.makeVirtualPlayer(); // to be removed
+
+		me = gameLogic.makePlayer(navn);
+		gameLogic.makeVirtualPlayer(); // to be removed
 		Application.launch(Gui.class);
 	}
 }
