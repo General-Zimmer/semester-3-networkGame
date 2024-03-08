317227a6e97c6c1611f8f454ff678a91091de608
Remy <rumena.mp@gmail.com>
2024-03-08 09:54:47 +0100

    gitignore updated

--- src/spil/version1/server/Server.java
+++ src/spil/version1/server/Server.java
@@ -3,9 +3,7 @@ package spil.version1.server;
 import spil.version1.gamefiles.ConcurrentArrayList;
 import spil.version1.gamefiles.GameLogic;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.PriorityQueue;
@@ -81,6 +79,10 @@ public class Server {
             }
         }
 		return size;
+	}
+
+	private static void sendBytesBack(){
+
 	}
 
 }
