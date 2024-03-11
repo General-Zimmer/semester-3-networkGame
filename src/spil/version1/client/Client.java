55612b570bd5e9d10f656d69399ec7bea52d9551
victor <au670070@uni.au.dk>
2024-03-11 08:40:55 +0100

    nye prints

--- src/spil/version1/client/Client.java
+++ src/spil/version1/client/Client.java
@@ -51,22 +51,10 @@ public class Client{
 			if(message.startsWith("tilmeldt " + navnGlobal)){
 				wait=false;
 			}else if(message.equals(navn+ " afvist")){
-				System.out.println("Server afviste spiller");
 				return;
 			}
 		}
 
-		System.out.println("stoppet med at vente");
-
-//		String stringRead = inFromServer.readLine();
-//		FileInputStream inputStream = new FileInputStream(stringRead);
-//		ObjectInputStream objectMap = new ObjectInputStream(inputStream);
-//		ConcurrentArrayList playersList = (ConcurrentArrayList) objectMap.readObject();
-
-
-		System.out.println("gui åbenet");
-
-		System.out.println("ind i uendelig loop");
 		while(true){
 			if (readBoardFromServer()) {
 				updateLocalBoard();
@@ -82,7 +70,6 @@ public class Client{
 		List<Player> playersList;
 
 
-		System.out.println("læser board nu");
 		try {
 			playersList = (List<Player>) objectInFromServer.readObject();
 		} catch (IOException e) {
@@ -91,13 +78,12 @@ public class Client{
             throw new RuntimeException(e);
         }
 
-		System.out.println("board læst " + playersList.size());
+		System.out.println("Antal spillere: " + playersList.size());
         serverBoard = playersList;
 		return true;
 	}
 
 	public static void sendMoveToServer(String move) {
-		System.out.println("sendte et move");
 		try {
 			outToServer.writeBytes("arnold " + navnGlobal+ " " + move+ "\n");
 			outToServer.flush();
@@ -106,29 +92,13 @@ public class Client{
 		}
 	}
 
-//	public static void updateLocalBoard2(){
-//		//TODO: opdater localGameLogic boarded, med gameLogics's updatePlayer metode...
-//		System.out.println("TESTLSEKTJL: " + serverBoard.size());
-//		for(int i = 0; i < serverBoard.size(); i++) {
-//			if(localLogic.getPlayer(serverBoard.asArrayList().get(i).getName()) == null) {
-//				Player playerServer = serverBoard.asArrayList().get(i);
-//				Player playerNew = localLogic.makePlayer(serverBoard.asArrayList().get(i).getName());
-//				playerNew.setLocation(playerServer.getLocation());
-//				System.out.println("ny spiller i spil");
-//			}
-//			int deltaX = localLogic.players.get(i).getXpos() - serverBoard.asArrayList().get(i).getXpos();
-//			int deltaY = localLogic.players.get(i).getYpos() - serverBoard.asArrayList().get(i).getYpos();
-//			localLogic.updatePlayer(serverBoard.asArrayList().get(i), deltaX, deltaY, serverBoard.asArrayList().get(i).getDirection());
-//			System.out.println("spillr opdateret i gui");
-//		}
-//	}
 	public static void updateLocalBoard(){
 		localLogic.players = serverBoard;
-		//TODO: opdater localGameLogic boarded, med gameLogics's updatePlayer metode...
-		System.out.println("TESTLSEKTJL: " + serverBoard.size());
+
 		for(int i = 0; i < localLogic.players.size();  i++) {
-			localLogic.updatePlayer(localLogic.players.get(i), 0, 0, serverBoard.get(i).getDirection());
-			System.out.println("spiller opdateret i gui");
+			Player p = localLogic.players.get(i);
+			localLogic.updatePlayer(p, 0, 0, serverBoard.get(i).getDirection());
+			System.out.println("Spiller: " + p.getName() + ", X: " + p.getXpos() + ", Y: " + p.getYpos());
 		}
 	}
 
